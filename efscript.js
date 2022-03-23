7e2227680172470fd2468dcb833b84ec7e6e7ce9
LethalLavaLand <53242896+LethalLavaLand@users.noreply.github.com>
2022-03-23 18:27:14 -0400

    Update efscript.js

--- efscript.js
+++ efscript.js
@@ -13,20 +13,46 @@ function delayStart() {
     //update
     //W();
     
-    const div1 = document.createElement('div');
-    const button = document.createElement('button');
-    button.innerText = 'Bungus';
-    button.addEventListener('click', () => {
-      alert('bingo bango');  
-    })
-    
-    div1.style.left = "700px";
     
     var b = document.body;
-    b.insertAdjacentElement("beforebegin", div1);
-    div1.appendChild(button);
+    const div1 = document.createElement('div');
+    div1.id = "ef1";
+    div1.style.position = "absolute";
+    div1.style.left = "1100px";
+    div1.style.top = "50px";
+    div1.style.boxSizing = "border-box";
+    b.insertAdjacentElement("beforeend", div1);
     
-    document.body.appendChild(button);
+    const button = document.createElement('button');
+    button.innerText = 'Bungus?';
+    button.addEventListener('click', () => {
+      console.log("test2");
+      //alert('bingo bango');  
+    })
+    button.style.position = "absolute";
+    button.style.width = "150px";
+    button.style.height = "50px";
+    button.style.fontSize = "150%";
+    document.getElementById("ef1").appendChild(button);
+    
+    const transcendTimeInput = document.createElement('input');
+    transcendTimeInput.id = "transcendTimeInput";
+    transcendTimeInput.style.position = "absolute";
+    transcendTimeInput.style.width = "150px";
+    transcendTimeInput.style.height = "50px";
+    transcendTimeInput.style.top = "50px";
+    transcendTimeInput.style.fontSize = "150%";
+    transcendTimeInput.style.left = "170px";
+    div1.appendChild(transcendTimeInput);
+    var x = document.getElementById("transcendTimeInput").value;
+    
+    const info1 = document.createElement('label');
+    info1.innerText = "Time for bungus:";
+    info1.style.position = "absolute";
+    info1.style.width = "150px";
+    info1.style.height = "50px";
+    info1.style.top = "50px";
+    info1.style.fontSize = "150%";
     
     //setInterval(scriptLoop, 1000);
 }
